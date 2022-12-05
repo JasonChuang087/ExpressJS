d117e2696000655668edb06f4cca177ade17b3d4
OhBlockByJames <jasonwalkman8@gmail.com>
2022-12-05 14:15:01 +0800

    remove pool.js

--- mysql.js
+++ mysql.js
@@ -6,39 +6,52 @@ const port = process.env.PORT || 3000
 app.use(express.static('./static'))
 const mysql = require('mysql');
 
-const con = mysql.createConnection({
+const pool  = mysql.createPool({
     host: "localhost",
     user: "user",
     password: "password",
-    database: "test",
+    database: "test"
 });
 
-let result = function( uid,err ) {
+let result = function( uid ) {
     return new Promise(( resolve, reject ) => {
-        // con.connect(function(err) {
-        if (err) {
-          reject( err )
-        } else {
-            con.query("SELECT * FROM users WHERE id=?", uid, (err, rows) => {
-            if ( err ) {
+        pool.getConnection(function(err, connection) {
+            if (err) 
+            {
+                // 取得可用連線出錯
                 reject( err )
-            } else {
-                resolve( rows )
             }
-          })
-        }
-    //   })
+            else
+            {
+                // 成功取得可用連線
+                // 使用取得的連線
+                connection.query( "SELECT * FROM users WHERE id=?",uid, (err, rows) => {
+                    // 使用連線查詢完資料
+                    if ( err ) {
+                        reject( err )
+                    } else {
+                        resolve( rows )
+                    }
+                    // 釋放連線
+                    connection.release();
+                    // 不要再使用釋放過後的連線了，這個連線會被放到連線池中，供下一個使用者使用
+                });
+            }
+        });
     })
 }
 
-app.get('/user/:id', function(req, res) { 
+app.get('/hello',(req,res)=>{
+    res.status(200).send('Hello world')
+})
+
+app.get('/user/:id', async function(req, res) { 
     console.log('ID:', req.params.id);
     const uid = req.params.id;
-    con.connect(async function(err) {
-        const data = await result(uid,err);
-        console.log(data[0]);
-        res.json({ user: data[0].name });
-    });
+    const data = await result(uid);
+    console.log(data[0]);
+    res.json({ user: data[0].name });
+
 })
 
 app.all('*',(req,res)=>{
